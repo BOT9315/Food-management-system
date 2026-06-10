afe89a19c53bd902d697d7a28a55d30063f6f7ae
Ankush <akrk053@gmail.com>
2026-06-10 11:01:38 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,13 @@
+
+menu = {
+    1: {'name': 'Pizza', 'price': 250.0},
+    2: {'name': 'Burger', 'price': 100.0},
+    3: {'name': 'Pasta', 'price': 180.0},
+    4: {'name': 'French Fries', 'price': 80.0},
+    5: {'name': 'Cold Drink', 'price': 40.0},
+    6: {'name': 'samosa', 'price': 20.0},
+}
+
+food_items = {}
+
+#add menu items acc. to your need
